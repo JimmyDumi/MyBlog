060f31c957716bee77a818a4879122ccdece9f91
JimmyDoomi <142150431+JimmyDumi@users.noreply.github.com>
2024-05-05 15:14:16 +0900

    Hello, GitHub

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="container" />;
+  return <div className="container">Hello, GitHub!!</div>;
 };
 
 export default App;
